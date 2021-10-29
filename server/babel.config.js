cd3f404445e80dd555e7c87a2d4f2cfebc4dab94
Gabriel Silva <gabrieldsilva@Gabriels-MacBook-Pro.local>
2021-10-29 00:59:03 -0400

    some prod config

---
+++ server/babel.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          "@modules": "./src/modules",
+          "@config": "./src/config",
+          "@common": "./src/common",
+        },
+      },
+    ],
+    "babel-plugin-transform-typescript-metadata",
+    ["@babel/plugin-proposal-decorators", { "legacy": true }],
+    ["@babel/plugin-proposal-class-properties", { "loose": true }],
+    ["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
+  ],
+};
